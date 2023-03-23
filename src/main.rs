fd71c9750543255dbd11c2e0e20e0cbb8f6d83b2
Andrei Dodu <andrei.dodu@hotmail.it>
2023-03-24 00:37:44 +0100

    chore: indentation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
 use select::document::Document;
 use select::predicate::Name;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
@@ -11,47 +11,55 @@ async fn main() {
     let links_map_arc: Arc<Mutex<HashMap<String, i64>>> = Arc::new(Mutex::new(HashMap::new()));
     let id_map_arc: Arc<Mutex<i64>> = Arc::new(Mutex::new(0));
     let mut handles = vec![];
-    
-    for _ in 0..10 { 
-        let links_map_arc= links_map_arc.clone();
+
+    for _ in 0..10 {
+        let links_map_arc = links_map_arc.clone();
         let id_map_arc = id_map_arc.clone();
         let handle = tokio::spawn(async move {
-
             println!("{}", id_map_arc.lock().unwrap());
-            let links: Vec<String> =  self::extract_links().await;
+            let links: Vec<String> = self::extract_links().await;
 
-            
-            links.iter().for_each(|item| { 
-                
+            links.iter().for_each(|item| {
                 let mut value = 0;
-                if links_map_arc.lock().unwrap().get(item).is_some(){
-                    value =  links_map_arc.lock().unwrap().get(item).unwrap() + 1;
+                if links_map_arc.lock().unwrap().get(item).is_some() {
+                    value = links_map_arc.lock().unwrap().get(item).unwrap() + 1;
                 }
-                
-                links_map_arc.lock().unwrap().insert(item.to_string(), value); });
+
+                links_map_arc
+                    .lock()
+                    .unwrap()
+                    .insert(item.to_string(), value);
+            });
         });
         handles.push(handle);
     }
-    
+
     for handle in handles {
         handle.await.unwrap();
     }
 
-    links_map_arc.lock().unwrap().iter().for_each(|item| println!("{}|{}", item.0.to_owned(), item.1));
+    links_map_arc
+        .lock()
+        .unwrap()
+        .iter()
+        .for_each(|item| println!("{}|{}", item.0.to_owned(), item.1));
     println!("=======================");
     println!("Done!");
     println!("=======================");
 }
 
-
-async fn extract_links()->Vec<String>{
+async fn extract_links() -> Vec<String> {
     let res = reqwest::get("https://www.rust-lang.org/en-US/")
-    .await.unwrap()
-    .text()
-    .await.unwrap();
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
 
-  let mut data : Vec<String> = Document::from(res.as_str())
-    .find(Name("a"))
-    .filter_map(|n| n.attr("href")).map(|item| item.to_string()).collect();
+    let mut data: Vec<String> = Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .map(|item| item.to_string())
+        .collect();
     return data;
 }

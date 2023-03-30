6ee90f2612803584c319600be57c78070278b19d
Andrei Dodu <andrei.dodu@hotmail.it>
2023-03-31 00:31:18 +0200

    feat: video extractor foundations

--- src/service/strategy_extractor/strategies/strategy_video_extractor.rs
+++ src/service/strategy_extractor/strategies/strategy_video_extractor.rs
@@ -1,5 +1,10 @@
+use select::{document::Document, predicate::Name};
+
 use crate::{
-    service::strategy_extractor::strategy_resource_extractor::ResourceExtractor,
+    service::{
+        strategy_extractor::strategy_resource_extractor::ResourceExtractor,
+        validation_service::{contains_extension, is_same_domain},
+    },
     structure::{
         config_struct::Config, domain_filter_struct::DomainFilter,
         extension_filter_struct::ExtensionFilter,
@@ -8,6 +13,7 @@ use crate::{
 
 pub struct VideoExtractor {}
 
+// TODO
 impl ResourceExtractor for VideoExtractor {
     fn extract(
         &self,
@@ -16,6 +22,20 @@ impl ResourceExtractor for VideoExtractor {
         domain_filter: &DomainFilter,
         extension_filter: &ExtensionFilter,
     ) -> Vec<String> {
-        return vec![];
+        return Document::from(resource_str)
+            .find(Name("source"))
+            .filter_map(|n| n.attr("src"))
+            .map(|item| item.to_string())
+            .filter_map(|link| {
+                is_same_domain(
+                    &domain_filter,
+                    extension_filter.is_resource_same_domain,
+                    &link,
+                )
+            })
+            .filter_map(|link| {
+                contains_extension(vec![".mp4".to_string(), ".ogg".to_string()], &link)
+            })
+            .collect();
     }
 }
